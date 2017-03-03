acd03fa9ab5c15ea8c631c428f6a9904c2fb8970
vnm <vnm3@alu.ua.es>
2017-03-03 12:16:19 +0100

    Re implementación de estructura

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# gestorSDS
+# Gestor de contraseñas - SDS
+
+## Comandos 
+
+### Lanzar servidor
+`go run app.go server`
+
+### Lanzar cliente
+`go run app.go client`
+
+### Construir proyecto
+`go build app.go`
--- server/server.go
+++ server/server.go
@@ -11,12 +11,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, database.GetAll())
 }
 
-// redirectToHttps redirecciona a conexión segura
-func redirectToHTTPS(w http.ResponseWriter, r *http.Request) {
-	// Redirect the incoming HTTP request. Note that "127.0.0.1:8081" will only work if you are accessing the server from your local machine.
-	http.Redirect(w, r, "https://127.0.0.1:8081"+r.RequestURI, http.StatusMovedPermanently)
-}
-
 // Launch lanza el servidor
 func Launch() {
 	// todo: borrar
@@ -26,9 +20,9 @@ func Launch() {
 	database.AddToUser("demoEmail", "twitter", "abcde")
 	database.AddUser("demoEmail2", "accountPass2")
 
-	http.HandleFunc("/", handler)
-	// Start the HTTPS server in a goroutine
-	go http.ListenAndServeTLS(":8081", "cert.pem", "key.pem", nil)
-	// Start the HTTP server and redirect all incoming connections to HTTPS
-	http.ListenAndServe(":8080", http.HandlerFunc(redirectToHTTPS))
+	http.HandleFunc("/hello", handler)
+	err := http.ListenAndServeTLS(":8021", "cert.pem", "key.pem", nil)
+	if err != nil {
+		panic(err)
+	}
 }
